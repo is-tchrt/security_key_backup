405a71f8c8764839b07b3d21aa7d93a9ffa6f01e
Work <work@debian.byu.edu>
2024-08-27 15:32:50 -0600

    Edited a print statement

--- libraries/opensk/src/ctap/mod.rs
+++ libraries/opensk/src/ctap/mod.rs
@@ -744,7 +744,7 @@ impl<E: Env> CtapState<E> {
             pin_uv_auth_protocol,
             enterprise_attestation,
         } = make_credential_params;
-        writeln!(env.write(), "Recovery: {:?}", None::<T>).unwrap();
+        writeln!(env.write(), "Recovery: {:?}", None::<bool>).unwrap();
 
         // let backup_data = BackupData::init(env);
         // writeln!(
