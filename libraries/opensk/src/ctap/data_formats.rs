dfa92c037b6015664fde63d56ed90c872ee91cdc
Work <work@debian.byu.edu>
2024-08-30 13:27:04 -0600

    Edited try_from cbor for RecoveryExtensionInput

--- libraries/opensk/src/ctap/data_formats.rs
+++ libraries/opensk/src/ctap/data_formats.rs
@@ -330,23 +330,24 @@ impl TryFrom<cbor::Value> for RecoveryExtensionInput {
             } = extract_map(cbor_value)?;
         }
 
-        let _rp_id = rp_id;
+        // let _rp_id = rp_id;
         // let _action = action;
         let _allow_list = allow_list_cbor;
-        let fake_rp = "action worked".to_string();
+        // let fake_rp = "action worked".to_string();
 
         let action = action
             .map(RecoveryExtensionAction::try_from)
             .transpose()?
             .unwrap();
 
+        let rp_id = rp_id.map(extract_text_string).transpose()?.unwrap();
+
         Ok(Self {
             // action: RecoveryExtensionAction::State,
             action,
-            rp_id: fake_rp,
+            rp_id,
             allow_list: None,
         })
-        // let rp_id = rp_id.map(extract_text_string).transpose()?.unwrap();
         // let allow_list_option = allow_list_cbor.map(extract_array).transpose()?;
         // if allow_list_option.is_some() {
         //     let old_allow_list = allow_list_option.unwrap();
